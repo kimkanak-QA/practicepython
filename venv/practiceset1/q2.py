875ed5328d5138c4fcf3584e2985d4839df567fb
Kanak K <kanakk@Kanaks-MacBook-Pro.local>
2025-10-16 02:32:33 +0530

    Q2 & Q4

---
+++ venv/practiceset1/q2.py
@@ -0,0 +1,20 @@
+'''
+
+
+Sum of all elements using a loop ➕
+
+🧩 Question
+
+Given:
+
+nums = [3, 5, 7, 9, 11]
+
+
+👉 Find the sum of all elements without using sum().
+'''
+
+nums = [3, 5, 7, 9, 11]
+sum = 0
+for n in nums:
+         sum += n
+print(sum)
---
+++ venv/practiceset1/q3.py
@@ -0,0 +1,16 @@
+'''
+
+Given a list of names:
+
+names = ["John", "Emma", "Olivia", "Liam"]
+
+
+✅ Task:
+Print each name along with the number of letters in that name.
+'''
+
+names = ["John", "Emma", "Olivia", "Liam"]
+
+for n in names:
+    ch = len(n)
+    print(ch,n)
