a3a5d8f542430e835117cc2385844bcaf5a965e2
Kanak K <kanakk@Kanaks-MacBook-Pro.local>
2025-10-16 02:13:21 +0530

    newfile

---
+++ venv/practiceset1/q1.py
@@ -0,0 +1,12 @@
+'''
+Q1️⃣
+
+Create a list of 10 numbers.
+👉 Print only those numbers which are even.
+'''
+
+number = [2, 5, 8, 3, 9, 21, 22, 88, 6]
+
+for n in number:
+    if n%2 == 0:
+        print(n)
